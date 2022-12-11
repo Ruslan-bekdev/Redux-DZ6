0cd2a4ca8ad26e5c6ff0d1431f983a57f04d87ac
Ruslan <toopikoliko2@gmail.com>
2022-12-11 21:10:04 +0600

    dz 6 complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import MainPage from "./pages/mainPage/MainPage";
-import UsersPage from "./pages/usersPage/UsersPage";
 import CalculatePage from "./pages/calculatePage/CalculatePage";
 
 function App() {
   return (
     <div>
-      {/*<MainPage/>*/}
-      {/*<UsersPage/>*/}
       <CalculatePage/>
     </div>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,8 @@
 import {configureStore} from "@reduxjs/toolkit";
-import titleReducer from "./titleSlice";
-import productReducer from "./productSlice";
 import totalReducer from "./totalSlice"
 
 export default configureStore({
   reducer:{
-    titleReducer,
-    productReducer,
     totalReducer,
   }
 })
